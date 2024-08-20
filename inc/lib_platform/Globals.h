25145a2309197b2eae1eed4433bdcbb93d93b2de
Tom Paynter <tompaynter@tdpe.co.uk>
2024-08-20 11:31:13 +0100

    Add a base amount of memory that we will give Blender.

--- inc/lib_platform/Globals.h
+++ inc/lib_platform/Globals.h
@@ -38,6 +38,66 @@
 #endif
 
 //##################################################################################################
+constexpr size_t operator""_KiB(unsigned long long int x)
+{
+  return 1024ULL * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_MiB(unsigned long long int x)
+{
+  return 1024_KiB * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_GiB(unsigned long long int x)
+{
+  return 1024_MiB * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_TiB(unsigned long long int x)
+{
+  return 1024_GiB * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_PiB(unsigned long long int x)
+{
+  return 1024_TiB * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_KB(unsigned long long int x)
+{
+  return 1000ULL * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_MB(unsigned long long int x)
+{
+  return 1000_KB * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_GB(unsigned long long int x)
+{
+  return 1000_MB * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_TB(unsigned long long int x)
+{
+  return 1000_GB * x;
+}
+
+//##################################################################################################
+constexpr size_t operator""_PB(unsigned long long int x)
+{
+  return 1000_TB * x;
+}
+
+//##################################################################################################
 //! Platform abstractions
 namespace lib_platform
 {
